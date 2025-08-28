c0f5a10518bc58f39fb4e699dc0e2af2646dcf22
Suhani Garg <suhanisg2005@gmail.com>
2025-08-29 01:03:34 +0530

    Update sumOfDigitOfN.py

--- sumOfDigitOfN.py
+++ sumOfDigitOfN.py
@@ -1,12 +1,9 @@
+num = int(input("Enter an integer: "))
+sum  = 0
 
-N = int(input("Enter an integer: "))
-
-sum_digits = 0
-num = abs(N)
-
-while num > 0:
+while (num > 0):
     digit = num % 10     
-    sum_digits += digit   
-    num //= 10            
+    sum = sum + digit   
+    num = num // 10            
 
-print("Sum of digits of", N, "is", sum_digits)
+print("Sum of digits = ", sum)
