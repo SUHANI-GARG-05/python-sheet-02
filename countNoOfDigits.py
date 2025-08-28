9cfdfbf73b50ee9522022cd4511acff11348eb2a
Suhani Garg <suhanisg2005@gmail.com>
2025-08-29 00:49:21 +0530

    Update countNoOfDigits.py

--- countNoOfDigits.py
+++ countNoOfDigits.py
@@ -1,12 +1,11 @@
-n = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
-if n == 0:
+if (num == 0):
     count = 1
 else:
     count = 0
-    num = abs(n)  
-    while num > 0:
+    while (num > 0):
         num //= 10  
-        count += 1
+        count = count + 1
 
 print("Number of digits:", count)
