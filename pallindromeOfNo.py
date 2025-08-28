c83206bbe7fe6e452ea1cf4a3549382019348ea6
Suhani Garg <suhanisg2005@gmail.com>
2025-08-29 01:06:39 +0530

    Update pallindromeOfNo.py

--- pallindromeOfNo.py
+++ pallindromeOfNo.py
@@ -3,12 +3,13 @@ num = int(input("Enter an integer: "))
 original = num
 reverse = 0
 
-while num > 0:
+while (num > 0):
     digit = num % 10
     reverse = reverse * 10 + digit
-    num //= 10
+    num = num // 10
 
-if original == reverse:
+if (original == reverse):
     print("Yes, it is a palindrome")
 else:
     print("No, it is not a palindrome")
+
